4d71aa91051d1493d0b74f962216039733e9c090
AIT-LAHCEN-REDOUAN <93483454+AIT-LAHCEN-REDOUAN@users.noreply.github.com>
2024-02-26 13:01:44 -0800

    Add Code

---
+++ demo/src/main/java/com/example/demo/student/StudentController.java
@@ -0,0 +1,16 @@
+package com.example.demo.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
+@RestController
+@RequestMapping(path = "api/v1/student")
+public class StudentController {
+    @GetMapping
+    public List<Student> getStudents(){
+        return List.of(new Student(1L,"ahmed","ahmed@gmail.com", LocalDate.of(2000,5,1),21));
+    }
+}
